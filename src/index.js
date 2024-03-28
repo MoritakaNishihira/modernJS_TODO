a5c380f528299b85af0d98b9cb638630c69ab5a6
moritaka nishihira <moritaka.nishihira@gmail.com>
2024-03-29 07:41:07 +0900

    いろいろ修正

--- src/index.js
+++ src/index.js
@@ -1,72 +1,64 @@
-(function () {
-  const onClickTodoItemAdd = () => {
-    const todoText = document.getElementById("todo_Input_Text").value;
+const onClickTodoItemAdd = () => {
+  const todoText = document.getElementById("todo_Input_Text").value;
+  if (todoText === "") return;
+  document.getElementById("todo_Input_Text").value = "";
+  createTodo(todoText);
+};
 
-    if (todoText === "") return;
-    document.getElementById("todo_Input_Text").value = "";
+const createTodo = (todoText) => {
+  const li = document.createElement("li");
+  const div_li = document.createElement("div");
+  div_li.classList.add("div-li-row");
+
+  const p = document.createElement("p");
+  p.classList.add("todo-item");
+  p.innerText = todoText;
+
+  div_li.appendChild(p);
+  div_li.appendChild(createCompleteButton());
+  div_li.appendChild(createDeleteButton());
+
+  li.appendChild(div_li);
+
+  return document.getElementById("ul_Incomlete").appendChild(li);
+};
+
+const createCompleteButton = () => {
+  const btn = document.createElement("button");
+  btn.innerText = "完了";
+  btn.addEventListener("click", () => {
+    const moveTarget = btn.closest("li");
+    btn.nextElementSibling.remove();
+    btn.remove();
+
+    moveTarget.firstElementChild.appendChild(createBackButton());
+    return document.getElementById("ul_Comlete").appendChild(moveTarget);
+  });
+  return btn;
+};
+
+const createDeleteButton = () => {
+  const btn = document.createElement("button");
+  btn.innerText = "削除";
+  btn.addEventListener("click", () => {
+    const deleteTarget = btn.closest("li");
+    document.getElementById("ul_Incomlete").removeChild(deleteTarget);
+  });
+  return btn;
+};
+
+const createBackButton = () => {
+  const btn = document.createElement("button");
+  btn.innerText = "戻す";
+  btn.addEventListener("click", () => {
+    const todoText = btn.previousElementSibling.innerText;
     createTodo(todoText);
-  };
+    const deleteTarget = btn.closest("li");
+    document.getElementById("ul_Comlete").removeChild(deleteTarget);
+  });
+  return btn;
+};
 
-  const createTodo = (todoText) => {
-    const li = document.createElement("li");
-    const div_li = document.createElement("div");
-    div_li.classList.add("div-li-row");
-
-    const p = document.createElement("p");
-    p.classList.add("todo-item");
-    p.innerText = todoText;
-
-    const btn_Complete = completeTodo();
-    const btn_Delete = deleteTodo();
-
-    div_li.appendChild(p);
-    div_li.appendChild(btn_Complete);
-    div_li.appendChild(btn_Delete);
-
-    li.appendChild(div_li);
-
-    return document.getElementById("ul_Incomlete").appendChild(li);
-  };
-
-  const completeTodo = () => {
-    const btn_Complete = document.createElement("button");
-    btn_Complete.innerText = "完了";
-    btn_Complete.addEventListener("click", () => {
-      const moveTarget = btn_Complete.closest("li");
-      btn_Complete.nextElementSibling.remove();
-      btn_Complete.remove();
-
-      const btn_Back = backTodo();
-
-      moveTarget.firstElementChild.appendChild(btn_Back);
-      return document.getElementById("ul_Comlete").appendChild(moveTarget);
-    });
-    return btn_Complete;
-  };
-
-  const deleteTodo = () => {
-    const btn_Delete = document.createElement("button");
-    btn_Delete.innerText = "削除";
-    btn_Delete.addEventListener("click", () => {
-      const deleteTarget = btn_Delete.closest("li");
-      document.getElementById("ul_Incomlete").removeChild(deleteTarget);
-    });
-    return btn_Delete;
-  };
-
-  const backTodo = () => {
-    const btn_Back = document.createElement("button");
-    btn_Back.innerText = "戻す";
-    btn_Back.addEventListener("click", () => {
-      const todoText = btn_Back.previousElementSibling.innerText;
-      createTodo(todoText);
-      const deleteTarget = btn_Back.closest("li");
-      document.getElementById("ul_Comlete").removeChild(deleteTarget);
-    });
-    return btn_Back;
-  };
-
-  document
-    .getElementById("button_Todo_Item_Add")
-    ?.addEventListener("click", onClickTodoItemAdd);
-})();
+document
+  .getElementById("button_Todo_Item_Add")
+  ?.addEventListener("click", onClickTodoItemAdd);
